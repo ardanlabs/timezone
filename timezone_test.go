cc269edb8cfa1f4320ea7656b5a14f1252616830
William Kennedy <bill@ardanlabs.com>
2019-08-15 11:48:15 -0400

    initial code

---
+++ timezone_test.go
@@ -0,0 +1,38 @@
+// Package timezone tests the different timezone API calls.
+package timezone_test
+
+import (
+	"fmt"
+
+	"github.com/ardanlabs/timezone"
+)
+
+// ExampleRetrieveGoogleTimezone tests the call to the google timezone API.
+func ExampleRetrieveGoogleTimezone() {
+	googleTimezone, err := timezone.RetrieveGoogleTimezone(25.7877, -80.2241)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%v", googleTimezone)
+	// Output:
+	// &{3600 -18000 OK America/New_York Eastern Daylight Time}
+}
+
+// ExampleRetrieveGeoNamesTimezone tests the call to the geonames timezone API.
+func ExampleRetrieveGeoNamesTimezone() {
+	geoNamesTimezone, err := timezone.RetrieveGeoNamesTimezone(25.7877, -80.2241, "ardanstudios")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	geoNamesTimezone.Time = "2015-12-17 13:13"
+	geoNamesTimezone.Sunrise = "2015-12-17 13:13"
+	geoNamesTimezone.Sunset = "2015-12-17 13:13"
+
+	fmt.Printf("%v", geoNamesTimezone)
+	// Output:
+	// &{2015-12-17 13:13 United States US 2015-12-17 13:13 -5 -4 -5 2015-12-17 13:13 America/New_York 0 0}
+}
